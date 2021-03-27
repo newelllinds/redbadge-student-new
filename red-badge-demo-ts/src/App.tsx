7aea7ad9ce06631f2660c43d3821221b61d5fa55
Exhale Yoga <exhaleyoga@exhales-air.lan>
2021-03-27 16:19:11 -0400

    dice project

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainPage from './components/MainPage'
 
 function App() {
   return (
     <div className="container">
-      <p>Hi from App.tsx</p>
+      {/* <p>Hi from App.tsx</p> */}
+      <Header />
+      <MainPage />
+      <Footer />
     </div>
   );
 }
---
+++ red-badge-demo-ts/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface FooterProps {
+    
+}
+ 
+const Footer: React.SFC<FooterProps> = () => {
+    return ( <div>Let Them Roll! 🎲</div> );
+}
+ 
+export default Footer;
